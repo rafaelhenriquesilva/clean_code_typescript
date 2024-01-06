a9f4b780a87eb2a8d6dc67e05b51e7d8dfa6e469
Rafael Candido <rafaelhenriquesilva494@gmail.com>
2024-01-06 09:35:38 -0300

    CleanCode + Typescript No delete cache when initialize instance of class Cache store

--- src/domain/data/usecases/local-save-purchases.spec.ts
+++ src/domain/data/usecases/local-save-purchases.spec.ts
@@ -1,5 +1,21 @@
+class LocalSavePurchases {
+    constructor(
+        private readonly cacheStore: CacheStore
+    ) { }
+}
+
+interface CacheStore {
+
+}
+
+class CacheStoreSpy implements CacheStore {
+    deleteCallsCount = 0
+}
+
 describe('LocalSavePurchases', () => {
-    it('', () => {
-        expect(1).toBe(1)
+    it('Should not delete cache on sut.init', () => {
+        const cacheStore = new CacheStoreSpy()
+        const sut = new LocalSavePurchases(cacheStore)
+        expect(cacheStore.deleteCallsCount).toBe(0)
     })
 })
