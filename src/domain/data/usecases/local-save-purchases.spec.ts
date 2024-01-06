92ce32be61b5e2c1d9c389fda23c0706d8475c72
Rafael Candido <rafaelhenriquesilva494@gmail.com>
2024-01-06 09:42:01 -0300

    Ensure LocalSavePurchases delete old cache on save

--- src/domain/data/usecases/local-save-purchases.spec.ts
+++ src/domain/data/usecases/local-save-purchases.spec.ts
@@ -2,20 +2,34 @@ class LocalSavePurchases {
     constructor(
         private readonly cacheStore: CacheStore
     ) { }
+
+    async save(): Promise<void> {
+        this.cacheStore.delete()
+    }
 }
 
 interface CacheStore {
-
+    delete(): void
 }
 
 class CacheStoreSpy implements CacheStore {
     deleteCallsCount = 0
+
+    delete(): void {
+        this.deleteCallsCount++
+    } 
 }
 
 describe('LocalSavePurchases', () => {
     it('Should not delete cache on sut.init', () => {
         const cacheStore = new CacheStoreSpy()
-        const sut = new LocalSavePurchases(cacheStore)
+        new LocalSavePurchases(cacheStore)
         expect(cacheStore.deleteCallsCount).toBe(0)
+    })
+    it('Should delete old cache on sut.save', async () => {
+        const cacheStore = new CacheStoreSpy()
+        const sut = new LocalSavePurchases(cacheStore)
+        await sut.save()
+        expect(cacheStore.deleteCallsCount).toBe(1)
     })
 })
