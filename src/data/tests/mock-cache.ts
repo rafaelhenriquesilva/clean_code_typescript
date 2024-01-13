e625f9628c9eb7adf5a4397b94a4abb3e0f19f4b
Rafael Candido <rafaelhenriquesilva494@gmail.com>
2024-01-13 09:19:09 -0300

    change asserts to use array to ensure order quantity od calls

--- src/data/tests/mock-cache.ts
+++ src/data/tests/mock-cache.ts
@@ -2,28 +2,40 @@ import { CacheStore } from '@/data/protocols/cache'
 import { SavePurchases } from '@/domain/usecases/save-purchases.usecase'
 
 export class CacheStoreSpy implements CacheStore {
+    messages: Array<CacheStoreSpy.Message> = []
     deleteKey: string
     insertKey: string
-    deleteCallsCount = 0
-    insertCallCount = 0
     insertValues: Array<SavePurchases.Params> = []
 
     insert(key: string, value:any): void {
-        this.insertCallCount++
+        this.messages.push(CacheStoreSpy.Message.insert)
         this.insertKey = key
         this.insertValues = value
     }
 
     delete(key: string): void {
+        this.messages.push(CacheStoreSpy.Message.delete)
         this.deleteKey = key
-        this.deleteCallsCount++
     }
 
     simulateDeleteError(): void {
-        jest.spyOn(CacheStoreSpy.prototype, 'delete').mockImplementationOnce(() => { throw new Error() })
+        jest.spyOn(CacheStoreSpy.prototype, 'delete').mockImplementationOnce(() => { 
+            this.messages.push(CacheStoreSpy.Message.delete)
+            throw new Error()
+         })
     }
 
     simulateInsertError(): void {
-        jest.spyOn(CacheStoreSpy.prototype, 'insert').mockImplementationOnce(() => { throw new Error() })
+        jest.spyOn(CacheStoreSpy.prototype, 'insert').mockImplementationOnce(() => { 
+            this.messages.push(CacheStoreSpy.Message.insert)
+            throw new Error()
+         })
+    }
+}
+
+export namespace CacheStoreSpy {
+    export enum Message {
+        delete,
+        insert
     }
 }
--- src/data/usecases/save-purchases/local-save-purchases.spec.ts
+++ src/data/usecases/save-purchases/local-save-purchases.spec.ts
@@ -19,22 +19,26 @@ const makeSut = (): SutTypes => {
 
 
 describe('LocalSavePurchases', () => {
-    it('Should not delete cache on sut.init', () => {
+    it('Should not delete cache or insert on sut.init', () => {
         const { cacheStore } = makeSut()
-        expect(cacheStore.deleteCallsCount).toBe(0)
+        expect(cacheStore.messages).toEqual([])
     })
     it('Should delete old cache on sut.save', async () => {
         const { sut, cacheStore } = makeSut()
         await sut.save(mockPurchases())
-        expect(cacheStore.deleteCallsCount).toBe(1)
+        expect(cacheStore.messages).toEqual([
+            CacheStoreSpy.Message.delete, CacheStoreSpy.Message.insert
+        ])
         expect(cacheStore.deleteKey).toBe('purchases')
     })
 
-    it('Should not insert not save new cache if delete faiils', async () => {
+    it('Should not insert new cache if delete faiils', async () => {
         const { sut, cacheStore } = makeSut()
         cacheStore.simulateDeleteError()
         const promise = sut.save(mockPurchases())
-        expect(cacheStore.insertCallCount).toBe(0)
+        expect(cacheStore.messages).toEqual([
+            CacheStoreSpy.Message.delete
+        ])
         expect(promise).rejects.toThrow()
     })
 
@@ -42,8 +46,9 @@ describe('LocalSavePurchases', () => {
         const { sut, cacheStore } = makeSut()
         const purchases = mockPurchases()
         await sut.save(purchases)
-        expect(cacheStore.deleteCallsCount).toBe(1)
-        expect(cacheStore.insertCallCount).toBe(1)
+        expect(cacheStore.messages).toEqual([
+            CacheStoreSpy.Message.delete, CacheStoreSpy.Message.insert
+        ])
         expect(cacheStore.insertKey).toBe('purchases')
 
         expect(cacheStore.insertValues).toBe(purchases)
@@ -53,6 +58,9 @@ describe('LocalSavePurchases', () => {
         const { sut, cacheStore } = makeSut()
         cacheStore.simulateInsertError()
         const promise = sut.save(mockPurchases())
+        expect(cacheStore.messages).toEqual([
+            CacheStoreSpy.Message.delete, CacheStoreSpy.Message.insert
+        ])
         expect(promise).rejects.toThrow()
     })
 })
